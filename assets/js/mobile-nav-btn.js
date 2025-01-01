9c47628e988c728ec61f653453f81a00581da2b9
jobcho <jobchojiyo@gmail.com>
2025-01-01 16:01:25 +0900

    update main mobile css

--- assets/js/mobile-nav-btn.js
+++ assets/js/mobile-nav-btn.js
@@ -1,7 +1,17 @@
 const $mobileNavBtn = document.querySelector(".mobile-bottom-area");
+const $dimmedLayer = document.querySelector(".dimmed-layer");
 $mobileNavBtn.addEventListener("click", () => {
   console.log("dee");
   const $mobileAllProject = document.querySelector(".mobile-all-project");
 
   $mobileAllProject.style.display = "block";
+  $dimmedLayer.style.display = "block";
+});
+
+$dimmedLayer.addEventListener("click", () => {
+  console.log("close");
+  const $mobileAllProject = document.querySelector(".mobile-all-project");
+
+  $mobileAllProject.style.display = "none";
+  $dimmedLayer.style.display = "none";
 });
