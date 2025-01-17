66034d61ad19888d4200ffc5b871d9c96fa09f17
jobcho <jobchojiyo@gmail.com>
2025-01-17 17:48:30 +0900

    update css

--- assets/js/mobile-bottom-shadow.js
+++ assets/js/mobile-bottom-shadow.js
@@ -2,14 +2,12 @@ mobileBottomShadow = () => {
   const $mobileNavBtn = document.querySelector(".mobile-bottom-area");
 
   if (
-    document.documentElement.scrollTop + window.innerHeight >=
+    document.documentElement.scrollTop + window.innerHeight + 0.5 >=
     document.body.scrollHeight
   ) {
     $mobileNavBtn.classList.add("no-shadow");
   } else {
     $mobileNavBtn.classList.remove("no-shadow");
   }
-
-  //window.removeEventListener("scroll", mobileBottomShadow);
 };
 window.addEventListener("scroll", mobileBottomShadow);
--- assets/js/mobile-nav-btn.js
+++ assets/js/mobile-nav-btn.js
@@ -1,9 +1,11 @@
 const $mobileNavBtn = document.querySelector(".mobile-bottom-area");
 const $dimmedLayer = document.querySelector(".dimmed-layer");
+const $mobileBtmArrowBtn = document.querySelector(".mobile-bottom-arrow");
 const $body = document.querySelector("body");
 $mobileNavBtn.addEventListener("click", () => {
   const $mobileAllProject = document.querySelector(".mobile-all-project");
   $body.classList.add("modal-open");
+  $mobileBtmArrowBtn.classList.add("down");
   $mobileAllProject.style.display = "block";
   $dimmedLayer.style.display = "block";
 });
@@ -11,7 +13,7 @@ $mobileNavBtn.addEventListener("click", () => {
 $dimmedLayer.addEventListener("click", () => {
   const $mobileAllProject = document.querySelector(".mobile-all-project");
   $body.classList.remove("modal-open");
-
+  $mobileBtmArrowBtn.classList.remove("down");
   $mobileAllProject.style.display = "none";
   $dimmedLayer.style.display = "none";
 });
