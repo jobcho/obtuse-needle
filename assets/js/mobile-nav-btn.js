06eecdc53de829529eaecb56abc493bcbaccd0b8
jobcho <jobchojiyo@gmail.com>
2025-01-17 18:03:26 +0900

    update css

--- assets/js/mobile-nav-btn.js
+++ assets/js/mobile-nav-btn.js
@@ -1,13 +1,20 @@
-const $mobileNavBtn = document.querySelector(".mobile-bottom-area");
+const $mobileNavBtn = document.querySelector(".mobile-bottom-nav-btn");
 const $dimmedLayer = document.querySelector(".dimmed-layer");
 const $mobileBtmArrowBtn = document.querySelector(".mobile-bottom-arrow");
 const $body = document.querySelector("body");
 $mobileNavBtn.addEventListener("click", () => {
   const $mobileAllProject = document.querySelector(".mobile-all-project");
-  $body.classList.add("modal-open");
-  $mobileBtmArrowBtn.classList.add("down");
-  $mobileAllProject.style.display = "block";
-  $dimmedLayer.style.display = "block";
+  if ($body.classList.contains("modal-open")) {
+    $body.classList.remove("modal-open");
+    $mobileBtmArrowBtn.classList.remove("down");
+    $mobileAllProject.style.display = "none";
+    $dimmedLayer.style.display = "none";
+  } else {
+    $body.classList.add("modal-open");
+    $mobileBtmArrowBtn.classList.add("down");
+    $mobileAllProject.style.display = "block";
+    $dimmedLayer.style.display = "block";
+  }
 });
 
 $dimmedLayer.addEventListener("click", () => {
